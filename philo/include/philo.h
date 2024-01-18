711f68f2c8afb7d1cd505804ff618cba58a71d53
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-01-18 23:52:12 +0100

    add ft_atoul, is_space and is_digit

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:44:00 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/01/18 22:30:37 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/01/18 23:47:59 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,26 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdbool.h>
+# include <limits.h>
+
 # include "colors.h"
 
 enum	e_err_msg
 {
 	WRONG_INPUT,
 	NO_MSG,
+	NEG_NUM,
+	NOT_NUM,
+	TOO_BIG,
 };
+
+typedef struct s_data
+{
+	unsigned long	nb_philo;
+	unsigned long	time_to_die;
+	unsigned long	time_to_eat;
+	unsigned long	time_to_sleep;
+	unsigned long	nb_meals;
+}	t_data;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:45:31 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/01/18 22:33:46 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/01/18 23:50:48 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,76 @@ void	error_handler(int err, bool is_exit)
 		printf("%s[WRONG INPUT]\n\t%sUsage:%s\n\t./philo [number_of_philosophers] \
 [time_to_die] [time_to_eat] [time_to_sleep] [number_of_meals](opt)\n\
 \t%se.g. ./philo 5 800 200 200 [10]%s\n", RED, UYEL, RST, GRN, RST);
+	else if (err == NEG_NUM)
+		printf("%s[WRONG INPUT]%s\n\tPlease enter only positive numbers.\n",
+			RED, RST);
+	else if (err == NOT_NUM)
+		printf("%s[WRONG INPUT]%s\n\tPlease enter only numbers.\n", RED, RST);
+	else if (err == TOO_BIG)
+		printf("%s[WRONG INPUT]%s\n\tPlease enter a number between 0 and 2147483647.\
+		 \n", RED, RST);
 	if (is_exit)
 		exit(EXIT_FAILURE);
 }
 
-bool	input_parser(char **av)
+static inline bool	is_space(char c)
 {
-	(void)av;
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+static inline bool	is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+const char	*check_input(const char *str)
+{
+	const char	*nb;
+	int			len;
+
+	len = 0;
+	while (is_space(*str))
+		++str;
+	if (*str == '+')
+		++str;
+	else if (*str == '-')
+		error_handler(NEG_NUM, true);
+	if (!is_digit(*str))
+		error_handler(NOT_NUM, true);
+	nb = str;
+	while (is_digit(*str++))
+		++len;
+	if (len > 10)
+		error_handler(TOO_BIG, true);
+	return (nb);
+}
+
+unsigned long	ft_atoul(const char *str)
+{
+	unsigned long	nb;
+
+	nb = 0;
+	str = check_input(str);
+	while (is_digit(*str))
+		nb = (nb * 10) + (*str++ - '0');
+	if (nb > INT_MAX)
+		error_handler(TOO_BIG, true);
+	return (nb);
+}
+
+bool	input_parser(char **av, t_data *data)
+{
+	data->nb_philo = ft_atoul(av[1]);
 	return (true);
 }
 
 int	main(int ac, char **av)
 {
+	t_data	data;
+
 	if (ac == 5 || ac == 6)
 	{
-		if (!input_parser(av)) //TODO: start creating struct, atoui and asignt values
+		if (!input_parser(av, &data)) //TODO: start creating struct, atoui and asignt values
 			error_handler(NO_MSG, true);
 	}
 	else
