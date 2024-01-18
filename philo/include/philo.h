c4b4bc6d2f087498200dec35323a97f3e635f346
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-01-18 22:40:24 +0100

    add colors, exit_handler

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,13 +6,22 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:44:00 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/01/18 15:46:26 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/01/18 22:30:37 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
+# include "colors.h"
+
+enum	e_err_msg
+{
+	WRONG_INPUT,
+	NO_MSG,
+};
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,14 +6,36 @@
 /*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 15:45:31 by sebasnadu         #+#    #+#             */
-/*   Updated: 2024/01/18 18:03:30 by sebasnadu        ###   ########.fr       */
+/*   Updated: 2024/01/18 22:33:46 by sebasnadu        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int	main(void)
+void	error_handler(int err, bool is_exit)
 {
-	printf("Hello Wordl!\n");
+	if (err == WRONG_INPUT)
+		printf("%s[WRONG INPUT]\n\t%sUsage:%s\n\t./philo [number_of_philosophers] \
+[time_to_die] [time_to_eat] [time_to_sleep] [number_of_meals](opt)\n\
+\t%se.g. ./philo 5 800 200 200 [10]%s\n", RED, UYEL, RST, GRN, RST);
+	if (is_exit)
+		exit(EXIT_FAILURE);
+}
+
+bool	input_parser(char **av)
+{
+	(void)av;
+	return (true);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 5 || ac == 6)
+	{
+		if (!input_parser(av)) //TODO: start creating struct, atoui and asignt values
+			error_handler(NO_MSG, true);
+	}
+	else
+		error_handler(WRONG_INPUT, true);
 	return (0);
 }
